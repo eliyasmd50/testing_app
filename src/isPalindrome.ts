ad38cfdc2dfdb7385fbf7c4e23a40232d9a69506
Mohamed Eliyas <mohamed.eliyas@photon.com>
2025-03-03 14:15:19 +0530

    unit testing changes

---
+++ src/isPalindrome.ts
@@ -0,0 +1,23 @@
+class Palindrome  {
+    str: string;
+
+    // to declare the varibale with the type definintion
+    public constructor (str: string) {
+        this.str = str.toLowerCase();
+    }
+
+    public isPalindrome(): Boolean {
+        return this.str === this.reverseString();
+    }
+
+    public reverseString(): String {
+        const strToReverse: String = this.str.split("").reverse().join("")
+        // console.log(strToReverse);
+        return strToReverse;
+    }
+}
+
+// const palin = new Palindrome("HannaH");
+// console.log(palin.isPalindrome());
+
+module.exports = Palindrome;
--- util.ts
+++
@@ -1,3 +0,0 @@
-export default interface Palindrom {
-    str: string
-}
